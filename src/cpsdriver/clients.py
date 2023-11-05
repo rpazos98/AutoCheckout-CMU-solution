9f9a63358e32b76559f1a88a6d6331ac7ff8aa9c
Rodrigo Pazos <rodrigo.pazos98@gmail.com>
2023-11-05 20:45:23 -0300

    using src and src/resources

--- src/cpsdriver/clients.py
+++ src/cpsdriver/clients.py
@@ -6,7 +6,7 @@ from pymongo import MongoClient
 import requests
 from sh import mongorestore
 
-from cpsdriver.codec import DocObjectCodec
+from src.cpsdriver.codec import DocObjectCodec
 
 
 logger = logging.getLogger(__name__)  # pylint: disable=invalid-name
