92e2a7e4cf23eb236e7ea35dac13bf1a29d69edc
Rodrigo Pazos <57221686+rpazos98@users.noreply.github.com>
2023-11-06 23:01:13 -0300

    Removed and re-organized files (#7)

---
+++ src/main/python/evaluation.py
@@ -0,0 +1,164 @@
+# Code for Evaluation
+from BookKeeper import *
+from cashier import Cashier
+import json
+
+"""
+Groundtruth file contains pickup event and putback event separately.
+remove_putback_products() will remove the putback event as well as its corresponding pickup event,
+so that the ground truth only contains file result. 
+"""
+
+
+def remove_putback_products(gt_list):
+    for db in gt_list:
+        final_events = []
+        for event in db["events"]:
+            if event["putback"] == 0:  # pick up
+                final_events.append(event)
+            else:  # putback => remove from final_events the coresponding pickup event
+                for i in range(len(final_events)):
+                    final_event = final_events[i]
+                    if (
+                        final_event["observation"]["target_id"]
+                        != event["observation"]["target_id"]
+                    ):
+                        continue
+                    if (
+                        final_event["observation"]["products"][0]["id"]
+                        != event["observation"]["products"][0]["id"]
+                    ):
+                        continue
+                    del final_events[i]
+                    break
+        db["events"] = final_events
+
+
+"""
+1. Evaluate inventory status
+Metrics:
+    1. Precision: TP/(TP+FP)
+    2. Recall: TP/(TP+FN)
+2. Evaluate association accuracy
+Metrics:
+    1. Accuracy: num_correct_association/num_correct_products
+"""
+
+
+def evaluate_intenvory(dbs, gt_path):
+    # Load JSON groundtruth
+    with open(gt_path) as f:
+        gt_data = json.load(f)
+
+    gt_list = gt_data["lists"]
+    if not DEBUG:
+        remove_putback_products(gt_list)
+        # with open('gt_final.json', 'w') as outfile:
+        #     json.dump(gt_data, outfile)
+
+    # Metrics
+    tp, fp, tn, fn = 0, 0, 0, 0
+    overall_num_preds, overall_num_gt = 0, 0
+    for i in range(len(dbs)):
+        print("\n\nEvaluating database: ", dbs[i])
+        # Metrics per database
+        db_tp, db_fp, db_tn, db_fn = 0, 0, 0, 0
+        db_pred_counts, db_gt_counts = 0, 0
+
+        ########## Generate Prediction ##########
+        dbName = dbs[i]
+        myCashier = Cashier()
+        receipts = myCashier.process(dbName)
+
+        ########## Evaluate Ground truth ##########
+        gt_entry = gt_list[i]
+        # Find groundtruth entry for this database
+        tmp_i = 0
+        while gt_entry["dataset"] != dbName:
+            tmp_i += 1
+            gt_entry = gt_list[tmp_i]
+            assert tmp_i < len(gt_list)
+        assert gt_entry["dataset"] == dbName
+        event_list = gt_entry["events"]
+        for event in event_list:
+            gt_products = event["observation"]["products"]
+            gt_customerID = event["observation"]["target_id"]
+            # Find the corresponding customer receipts
+            if gt_customerID in receipts:
+                # Find the corresponding products on the receipt
+                customer_receipt = receipts[gt_customerID]
+                for gt_product in gt_products:
+                    gt_productID = gt_product["id"]
+                    for productID, entry in customer_receipt.purchaseList.items():
+                        if gt_productID == productID:
+                            product, quantity = entry
+                            if quantity > 0:
+                                db_tp += 1
+                                customer_receipt.purchaseList[productID] = (
+                                    product,
+                                    quantity - 1,
+                                )  # entry = (product, quantity)
+                    db_gt_counts += 1
+            else:
+                # No such customer
+                for gt_product in gt_products:
+                    db_gt_counts += 1
+
+        # Print False Items
+
+        num_receipt = 0
+        for id, customer_receipt in receipts.items():
+            if VERBOSE:
+                print(
+                    "============== False Receipt {} ==============".format(num_receipt)
+                )
+                print("Customer ID: " + id)
+                print("Purchase List: ")
+            for _, entry in customer_receipt.purchaseList.items():
+                product, quantity = entry
+                if quantity != 0:
+                    if VERBOSE:
+                        print(
+                            "*Name: " + product.name + ", Quantities: " + str(quantity),
+                            product.thumbnail,
+                        )
+                    db_fp += quantity
+            num_receipt += 1
+
+        db_fn = db_gt_counts - db_tp
+        db_pred_counts = db_tp + db_fp
+        overall_num_preds += db_pred_counts
+        overall_num_gt += db_gt_counts
+        tp += db_tp
+        fp += db_fp
+        fn += db_fn
+
+        # Display DB Evaluation
+        print(
+            "Database: {}, Correct Items on Receipts: {}/{}, Total GT items: {}".format(
+                dbName, db_tp, db_pred_counts, db_gt_counts
+            )
+        )
+
+    print("\n================== Evaluation Summary ==================")
+    print("Databases: ", dbs)
+    print("Ground truth version: ", gt_path)
+    precision = 0.0 if tp + fp == 0 else tp * 100.0 / (tp + fp)
+    print("Overall precision is: {:.1f}%".format(precision))
+    recall = 0.0 if tp + fn == 0 else tp * 100.0 / (tp + fn)
+    print("Overall recall is: {:.1f}%".format(recall))
+    if precision + recall == 0:
+        f1 = 0
+    else:
+        f1 = 2 * precision * recall / (precision + recall)
+    print("Overall F1 is: {:.1f}%".format(f1))
+
+
+if __name__ == "__main__":
+    # dbs =['cps-test-01', 'cps-test-2'] + ['cps-test-'+str(i) for i in range(4, 13)]
+    dbs = ["cps-test-2"]
+    # dbs += ['cps-test-19', 'cps-test-20','cps-test-21','cps-test-22','cps-test-23','cps-test-24'] # TODO: cps-test-23 exception
+    # dbs += ['ALL-SIMPLE-CHIP-1', 'TEAM-PEI-1', 'TEAM-PEI-JD-1', 'TEAM-8-1']
+    # dbs = ['BASELINE-1','BASELINE-2','BASELINE-3', 'BASELINE-4', 'BASELINE-5', 'BASELINE-6', 'BASELINE-7','BASELINE-8','BASELINE-10','BASELINE-11', 'BASELINE-12', 'BASELINE-13', 'BASELINE-14', 'BASELINE-16', 'BASELINE-20', 'BASELINE-22', 'BASELINE-23', 'BASELINE-25']
+    gt_path = "src/main/resources/ground_truth/v14.json"  # list of ground truth W.R.T the previous databases
+    evaluate_intenvory(dbs, gt_path)
---
+++ src/main/python/submit.py
@@ -0,0 +1,115 @@
+from cashier import Cashier, VideoCashier
+import subprocess
+import json
+import os
+from pprint import pprint
+
+
+def output_json(db_id, user, receipts, path):
+    print("=======================")
+    output = {}
+    output["testcase"] = db_id
+    output["user"] = user
+    receipts_json = []
+    for id_result in receipts:
+        receipt = receipts[id_result]
+        receipt_json = {}
+        receipt_json["target_id"] = id_result
+        products = []
+        for purchase in receipt.purchaseList:
+            product = {}
+            # Workaround for db error: [JD] You are correct on BASELINE-3 and BASELINE-11:
+            # The following product is scanned by our scanner with an extra "0":
+            if purchase == "120130":
+                productID = "01201303"
+            elif purchase == "120850":
+                productID = "0120850"
+            else:
+                productID = purchase
+            product["barcode"] = productID
+            product["quantity"] = receipt.purchaseList[purchase][1]
+            products.append(product)
+        receipt_json["products"] = products
+        receipts_json.append(receipt_json)
+    output["receipts"] = receipts_json
+    with open(path, "w") as outfile:
+        json.dump(output, outfile)
+
+
+def generate_output():
+    # Load JSON
+    f = open("../../../doc/competition/test_cases_specific.json")
+
+    test_cases = json.load(f)
+    myCashier = Cashier()
+    userID = "5ea023be-b530-4816-8eda-5340cfabe9b0"
+    output_paths = []
+    for test_db in test_cases:
+        print(test_db["name"])
+        dbName = test_db["name"]
+        dbId = test_db["uuid"]
+        receipts = myCashier.process(dbName)
+        myVideoCashier = VideoCashier(db_name=dbName)
+        video_receipts = myVideoCashier.process()
+        # Generate output file
+        path = "outputs/output-{}.json".format(dbName)
+        output_paths.append(path)
+        output_json(dbId, userID, receipts, path=path)
+    return output_paths
+
+
+def cal_avg(scores):
+    if len(scores) == 0:
+        return None
+    total = 0
+    for score in scores:
+        total += score
+    return total / len(scores)
+
+
+def get_score(output_paths=["outputs/output-BASELINE-1.json"]):
+    # Send a POST Request
+    # command_line = [
+    #     'curl', '--location', '--request' ,'POST', '\'cps-week.internal.aifi.io/api/v1/results\'',\
+    #     '--header', '\'TOKEN: 5ea023be-b530-4816-8eda-5340cfabe9b0\'',\
+    #     '--header', '\'Content-Type: application/json\'',\
+    #     '--data-raw'
+    # ]
+    pre_cmd = (
+        "curl --location --request POST 'cps-week.internal.aifi.io/api/v1/results' --header 'TOKEN: "
+        "5ea023be-b530-4816-8eda-5340cfabe9b0' --header 'Content-Type: application/json' --data-raw "
+    )
+    f1_scores = []
+    f = open("../../../doc/competition/results.md", "w")
+    # output_str = ""
+    for path in output_paths:
+        with open(path, "r") as file:
+            raw_string = file.read().replace("\n", "")
+        cmd = pre_cmd + "'" + raw_string + "'"
+        head_str = "\n=============== Receive feedbacks for %s ===============\n" % path
+        f.write(head_str)
+        print(head_str)
+        # output_str += head_str + '\n'
+        # Option 1 Record
+        stream = os.popen(cmd)
+        output = stream.read()
+        print(output)
+        output_dict = json.loads(output)
+        pprint(output_dict, f)
+        f1_scores.append(output_dict["f1_score"])
+
+    averageScoreStr = "Average F1 score over all test cases: %f\n" % (
+        cal_avg(f1_scores)
+    )
+    print(averageScoreStr)
+    f.write(averageScoreStr)
+    f.close()
+
+
+if __name__ == "__main__":
+    if not os.path.exists("../../../outputs"):
+        os.makedirs("../../../outputs")
+    output_paths = generate_output()
+    # output_paths = ['outputs/output-BASELINE-1.json', 'outputs/output-BASELINE-2.json']
+    print("Submitting: ", output_paths)
+    # get_score(output_paths)
